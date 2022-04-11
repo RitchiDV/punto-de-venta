2c7a8148c750021d895da1d564544211ad8f7f4a
Ricardo <hakdigital98@gmail.com>
2022-04-10 22:23:21 -0500

    funcion: para añadir y mostrar lista, actual y antigua

--- python3final/main.py
+++ python3final/main.py
@@ -1,24 +1,31 @@
-def inicio():
-    clientes = "pablo , ricardo ,"
-    clientes += "david"
-    print(clientes)
+from http import client
+
+
+clients = "pablo,\nricardo,\n"
+
+
+def create_client(client_name):
+    global clients
+    clients += client_name
+    add_comma()
+
+
+def list_clients():
+    global clients
+    print(clients)
+    
 
 
 
-
-
-
-
-
-
-
-
-
+def add_comma():
+    global clients
+    clients += ",\n"
 
     
 def run ():
-    inicio()
-
+    list_clients()
+    create_client(input("nombre del cliente nuevo:  "))
+    list_clients()
 
 
 
