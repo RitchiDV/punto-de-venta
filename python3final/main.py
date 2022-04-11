b209498b97a31d3bf9a5e3bdd0fdc11113dd848d
Ricardo <hakdigital98@gmail.com>
2022-04-10 21:25:30 -0500

    inicio de punto de venta

---
+++ python3final/main.py
@@ -0,0 +1,26 @@
+def inicio():
+    clientes = "pablo , ricardo ,"
+    clientes += "david"
+    print(clientes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+def run ():
+    inicio()
+
+
+
+
+if __name__=="__main__":
+    run()
